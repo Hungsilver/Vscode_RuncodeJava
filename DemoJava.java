7cfb04d930a206a353237d639c328918f5baf6ad
hung-silver <hungsilvertq@gmail.com>
2022-09-15 14:41:42 +0700

    add lan 1

---
+++ DemoJava.java
@@ -0,0 +1,11 @@
+
+public class DemoJava {
+
+    public static void main(String[] args) {
+        StringBuilder stb = new StringBuilder();
+        stb.append("xin chào ");
+        stb.append("bạn duy hưng ");
+        stb.append("đang học tại fpoly");
+        System.out.println(stb);
+    }
+}
